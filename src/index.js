bd7c22cf07f070dd700ba8295c2f70bdde3e8373
Ezra <ezraermy@gmail.com>
2023-03-01 21:20:17 +0300

    Fix todo list keys of an array of object

--- src/index.js
+++ src/index.js
@@ -1,32 +1,31 @@
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+const eachTodoList = [
+  {
+    description: 'cooking',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'Cleaning the house',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Going to gym',
+    completed: false,
+    index: 2,
+  },
+];
 
-  const eachTodoList = [
-    {
-      description: 'cooking',
-    },
-    {
-      description: 'Cleaning the house',
-    },
-    {
-      description: 'Going to gym',
-    },
-  ];
+const allTodoList = document.querySelector('#todoList');
+for (let i = 0; i < eachTodoList.length; i += 1) {
+  const todo = `
+    <li><p><span>${eachTodoList[i].completed}</span> | <span>${eachTodoList[i].description}</span> | <span>${eachTodoList[i].index}</span></p></li>
+    `;
 
-  const allTodoList = document.querySelector('#todoList');
-  for (let i = 0; i < eachTodoList.length; i += 1) {
-    const todo = `
-    <li><input type="checkbox" id="checkbox" name="todo">
-        <label for="scales">${eachTodoList[i].description}</label><button class="input-btn"><i class="fas fa-ellipsis-v"></i></button></li>
-        `;
-
-    allTodoList.innerHTML += todo;
-    allTodoList.innerHTML += '<hr>';
-  }
-
-  return element;
+  allTodoList.innerHTML += todo;
+  allTodoList.innerHTML += '<hr>';
 }
 
-document.body.appendChild(component());
+document.addEventListener('load', allTodoList);
